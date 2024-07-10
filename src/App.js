7d4f0b32791bb23594f3eed459cff24c584b9ea1
sachinadk2011 <adhikarisachin502@gmail.com>
2024-07-10 16:46:32 +0545

    changes in props country

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ export default class App extends Component {
   state = {
     progre: 0
   }
+  country = "in";
   setProgress=(progress)=>{
     this.setState({
       progre: progress
@@ -34,18 +35,18 @@ export default class App extends Component {
         
       />
          <Routes>
-         <Route exact path="/" element={<NewsComponent setProgress={this.setProgress}  apiKey={this.apiKey} key="all" country="in" pgSz={9} type=" " />} />
+         <Route exact path="/" element={<NewsComponent setProgress={this.setProgress}  apiKey={this.apiKey} key="all" country={this.country} pgSz={9} type=" " />} />
     
-          <Route exact path="/business" element={<NewsComponent setProgress={this.setProgress} apiKey={this.apiKey} key="business" country="in" pgSz={9} type="business" />} />
+          <Route exact path="/business" element={<NewsComponent setProgress={this.setProgress} apiKey={this.apiKey} key="business" country={this.country} pgSz={9} type="business" />} />
           
-          <Route exact path="/entertainment" element={<NewsComponent setProgress={this.setProgress} apiKey={this.apiKey} key="entainment" country="in" pgSz={9} type="entertainment" />} />
+          <Route exact path="/entertainment" element={<NewsComponent setProgress={this.setProgress} apiKey={this.apiKey} key="entainment" country={this.country} pgSz={9} type="entertainment" />} />
           
-          <Route exact path="/general" element={<NewsComponent setProgress={this.setProgress} apiKey={this.apiKey} key="general" country="in" pgSz={9} type="general" />} />
-          <Route exact path="/health" element={<NewsComponent setProgress={this.setProgress} apiKey={this.apiKey} key="health"  country="in" pgSz={9} type="health" />} />
+          <Route exact path="/general" element={<NewsComponent setProgress={this.setProgress} apiKey={this.apiKey} key="general" country={this.country} pgSz={9} type="general" />} />
+          <Route exact path="/health" element={<NewsComponent setProgress={this.setProgress} apiKey={this.apiKey} key="health"  country={this.country} pgSz={9} type="health" />} />
           
-          <Route exact path="/sports" element={<NewsComponent setProgress={this.setProgress} apiKey={this.apiKey} key="sports" country="in" pgSz={9} type="sports" />} />
-          <Route exact path="/science" element={<NewsComponent setProgress={this.setProgress} apiKey={this.apiKey} key="science"  country="in" pgSz={9} type="science" />} />
-          <Route exact path="/technology" element={<NewsComponent setProgress={this.setProgress} apiKey={this.apiKey} key="technology" country="in" pgSz={9} type="technology" />} />
+          <Route exact path="/sports" element={<NewsComponent setProgress={this.setProgress} apiKey={this.apiKey} key="sports" country={this.country} pgSz={9} type="sports" />} />
+          <Route exact path="/science" element={<NewsComponent setProgress={this.setProgress} apiKey={this.apiKey} key="science"  country={this.country} pgSz={9} type="science" />} />
+          <Route exact path="/technology" element={<NewsComponent setProgress={this.setProgress} apiKey={this.apiKey} key="technology" country={this.country} pgSz={9} type="technology" />} />
           
         </Routes>
       
